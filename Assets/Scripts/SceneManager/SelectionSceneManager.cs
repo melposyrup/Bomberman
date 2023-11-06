226159b1c495dfa934ea50e4b58fc900b789f1f4
22CU0329 <22CU0329@jec.ac.jp>
2023-11-06 11:31:39 +0900

    backup

--- Assets/Scripts/SceneManager/SelectionSceneManager.cs
+++ Assets/Scripts/SceneManager/SelectionSceneManager.cs
@@ -8,6 +8,11 @@ public class SelectionSceneManager : SceneManagerBase
 	public KeyCode InputRightArrow = KeyCode.RightArrow;
 	public KeyCode InputSelect = KeyCode.Return;
 
+	// 現在の選択画面
+	private int _selectScene = 0;
+	// 現在選んでいる場所
+	public int SelectNum = 0;
+
 	[SerializeField] private FadingImage _fadingImage;
 
 	private void Start()
@@ -17,9 +22,47 @@ public class SelectionSceneManager : SceneManagerBase
 
 	private void Update()
 	{
-		//player model selection
+		switch (_selectScene)
+		{
+			//select number of players
+			// プレイヤー数を選択(実装するかわからない)
+			case 0:
+				_selectScene++;
+					break;
+			//player model selection
+			// プレイヤーキャラを選択
+			case 1:
+				SelectPlayerModel();
+				break;
+			// change scene
+			// バトルシーンに変更
+			case 2:
+				// if all the players are selected, go to game scene
+				{
+					SceneChange(SceneManagerBase.EScene.GameScene);
+				}
+				break;
+		}
+	}
 
-		// if all the players are selected, go to game scene
+	// select players
+	void SelectPlayerModel()
+    {		
+		// Button Input
+		if (Input.GetKeyDown(InputLeftArrow))
+		{
+			SelectNum--;
+		}
+		else if (Input.GetKeyDown(InputRightArrow))
+		{
+			SelectNum++;
+		}
+		else if (Input.GetKeyDown(InputSelect))
+		{
+			_selectScene++;
+		}
+
+		
 	}
 
 }
---
+++ Assets/Scripts/UI/ArrowManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowManager : MonoBehaviour
+{
+	public GameObject SelMng;
+	SelectionSceneManager _selNum;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		SelMng = GameObject.Find("SelectionManager");
+		_selNum = SelMng.GetComponent<SelectionSceneManager>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		// Move Arrow
+		if (_selNum.SelectNum == 0)
+		{
+			transform.position = new Vector3(-7.5f, 2, 0);
+		}
+		else if (_selNum.SelectNum == 1)
+		{
+			transform.position = new Vector3(-2.5f, 2, 0);
+		}
+		else if (_selNum.SelectNum == 2)
+		{
+			transform.position = new Vector3(2.5f, 2, 0);
+		}
+		else if (_selNum.SelectNum == 3)
+		{
+			transform.position = new Vector3(7.5f, 2, 0);
+		}
+		else if (_selNum.SelectNum < 0)
+		{
+			_selNum.SelectNum = 3;
+		}
+		else if (_selNum.SelectNum > 3)
+		{
+			_selNum.SelectNum = 0;
+		}
+
+	}
+}
